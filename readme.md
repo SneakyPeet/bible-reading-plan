1592fd1672b9fcbae3bc2ba16a0b648dcc64e0fb
Pieter Koornhof <koornhof.pieter@gmail.com>
2023-09-24 15:07:39 +0200

    fin

---
+++ readme.md
@@ -0,0 +1,14 @@
+A web app to help track reading of "Professor Grant Horner's Bible-Reading System"
+
+### Dev
+
+npx shadow-cljs watch frontend
+
+### Build
+
+npx shadow-cljs release frontend
+
+
+### Prod
+
+https://bible-reading-plan-8e594.web.app/
--- src/main/pghbr/frontend/components.cljs
+++ src/main/pghbr/frontend/components.cljs
@@ -3,42 +3,38 @@
             [reagent.core :as r]))
 
 
-(defonce *page (r/atom :reading))
+(defonce ^:private *page (r/atom ::reading))
 
-(defn show-stats! []
-  (reset! *page :stats))
+(defn- show-stats! []
+  (reset! *page ::stats))
 
-(defn show-reading! []
-  (reset! *page :reading))
+(defn- show-reading! []
+  (reset! *page ::reading))
 
 
 (defn- day-selection [day]
   (let [col-style {:display "flex" :justify-content "center" :padding "0"}]
     [:div.row
      [:div.col-2.col {:style col-style}
-      [:button.btn-small
-       {:on-click #(show-stats!)
-        }
-       "?"]]
+      [:button.btn-small {:on-click #(show-stats!)} "?"]]
+
      [:div.col-2.col {:style col-style}
-      [:button.btn-small
-       {:on-click #(events/set-day! (dec day))
-        }
-       "<"]]
+      [:button.btn-small {:on-click #(events/set-day! (dec day))} "<"]]
+
      [:div.col-6.col {:style col-style}
       [:input {:type      "number"
                :value     day
                :min       1
                :on-change #(events/set-day! (js/parseInt (.. % -target -value)))
-               :style     {:max-height "2.1rem"}}]]
+               :style     {:max-height "2.1rem"
+                           :max-width "5rem"}}]]
+
      [:div.col-2.col {:style col-style}
-      [:button.btn-small {:on-click #(events/set-day! (inc day))}
-       ">"]]]))
+      [:button.btn-small {:on-click #(events/set-day! (inc day))} ">"]]]))
 
 
 (defn- progress-width [progress]
   (str "w-" (int progress)))
-
 
 
 (defn- progress-bar-book [progress]
@@ -65,52 +61,150 @@
                [:div.col.col-9 {:style col-style}
                 [:span list-number ". " book " " chapter]]
                [:div.col.col-3 {:style (assoc col-style :text-align "right")}
-                [:span.text-muted {:style {:font-size "0.6rem"}} total-chapters-book " | " total-chapters-list]]
+                [:span.text-muted {:style {:font-size "0.6rem"}}
+                 [:span.text-secondary total-chapters-book] " | "
+                 [:span.text-success total-chapters-list]]]
                [:div.col.col-12 {:style col-style}
                 [progress-bar-book completion-percentage-chapter ""]
                 [progress-bar-list completion-percentage-list ""]]])))
      #_[:pre {:style {:white-space "break-spaces"}} (str reading-list)]]))
 
 
-(defn round [decimal-places num]
+;; HELP
+
+(defn- round [decimal-places num]
   (let [multiplier (js/Math.pow 10 decimal-places)]
     (/ (js/Math.round (* num multiplier)) multiplier)))
 
-(defn- stats [{:keys [total-times-bible-read total-times-books-read]}]
+
+(defn- help []
   [:div
-   [:button.btn-small {:on-click #(show-reading!)} "Back"]
-   [:p "Professor Grant Horner's Bible-Reading System (" [:small [:a {:href "/download.pdf" :target "_blank"} "click to read more about it"]] ")"]
-   [:table
-    [:thead
-     [:tr [:th {:col-span 2} "You have read:"]]]
-    [:tbody
-     [:tr [:td "The Bible"] [:td {:style {:text-align "right"}} (round 2 total-times-bible-read) " times"]]]
-    [:tbody
-     (->> total-times-books-read
-          (map-indexed
-            (fn [i [_ book times]]
-              [:tr {:key i}
-               [:td book]
-               [:td {:style {:text-align "right"}} (round 2 times) " times"]])))]]
+   [:div "This app is an electronic bookmark for"
+    [:strong.text-secondary " Professor Grant Horner's Bible-Reading System"]]
+   [:h5 "How to use it?"]
+   [:ul
+    [:li "Read all 10 chapters as displayed on the page."]
+    [:li "Click '>' to load the chapters for the next days reading."]
+    [:li "The app saves the selected day on your current device."]
+    [:li "You can also type in the day you want to jump to."]
+    [:li "The blue progress bar shows chapter progress."]
+    [:li "The green progress bar shows list progress."]
+    [:li "Once you complete a list, that list will start over."]
+    [:li "For more information, the plan can be downloaded " [:a {:href "/download.pdf" :target "_blank"} "here"] "."]]])
 
-   [:div.border.border-2.padding.margin
-    [:span "Built using"]
-    [:ul
-     [:li "Clojurescript"]
-     [:li [:a {:href "https://shadow-cljs.github.io/"} "Shaddow-cljs"]]
-     [:li [:a {:href "https://www.getpapercss.com/"} "Paper Css"]]]]]
-  )
+(defn- back-to-reading []
+  [:button.btn-small {:on-click #(show-reading!)} "Back"])
 
+(defn- credits []
+  [:div
+   [:h5 "The app was built using"]
+   [:ul
+    [:li "Clojurescript"]
+    [:li [:a {:href "https://shadow-cljs.github.io/"} "Shaddow-cljs"]]
+    [:li [:a {:href "https://www.getpapercss.com/"} "Paper Css"]]]])
+
+
+(defn- about-page []
+  [:div
+   [help]
+   [credits]])
+
+
+;; STATS
+
+(defn- list-times-read-td [v]
+  [:td {:style {:text-align "right"}} (round 2 v) " times"])
+
+
+(defn- most-read-books-table [{:keys [total-times-books-read]}]
+  [:table
+   [:tbody
+    (->> total-times-books-read
+         (sort-by last)
+         reverse
+         (map-indexed
+           (fn [i [_ book times]]
+             [:tr {:key (str "t" i)}
+              [:td book]
+              [list-times-read-td times]])))]])
+
+
+(defn- stats-table [{:keys [total-times-bible-read total-times-books-read reading-list]}]
+  [:table
+   [:thead
+    [:tr [:th {:col-span 2} "You have read:"]]]
+   [:tbody
+    [:tr
+     [:td "The Bible"]
+     [list-times-read-td total-times-bible-read]]]
+   [:tbody
+      (->> reading-list
+         (map-indexed
+           (fn [i {:keys [list-number total-times-list-read]}]
+             [:tr {:key (str "l" i)}
+              [:td "List " list-number]
+              [list-times-read-td total-times-list-read]])))]
+   [:tbody
+    (->> total-times-books-read
+         (map-indexed
+           (fn [i [_ book times]]
+             [:tr {:key i}
+              [:td book]
+              [list-times-read-td times]])))]])
+
+
+;; TABS
+
+(defonce ^:private *stats-tab (r/atom ::stats-overall))
+
+(def ^:private tabs [[::stats-overall
+                      "Stats"
+                      stats-table]
+                     [::stats-books
+                      "Most Read"
+                      most-read-books-table]
+                     [::about
+                      "About"
+                      about-page]])
+
+(defn stats-tabs [stats]
+  (let [selected-tab @*stats-tab
+        render (some
+                 (fn [[k _ f]]
+                   (when (= selected-tab k)
+                     f))
+                 tabs)]
+    (prn selected-tab)
+    (prn render)
+    [:div
+     [:div.row.flex-spaces.tabs
+      (->> tabs
+           (map (fn [[k title _]]
+                  [:<> {:key title}
+                   [:input {:type     "radio"
+                            :name     "tabs"
+                            :checked  (= selected-tab k)
+                            :id       title
+                            :readOnly true
+                            :on-click #(reset! *stats-tab k)}]
+                   [:label {:for title} title]])))]
+     [render stats]]))
+
+
+(defn- help-page [stats-input]
+  [:div
+   [back-to-reading]
+   [stats-tabs stats-input]])
 
 
 (defn page [app-state]
   (case @*page
-    :reading
+    ::reading
     [:div
      [day-selection (:day app-state)]
      [reading-list (get-in app-state [:stats :reading-list])]]
 
-    :stats
-    [stats (:stats app-state)]
+    ::stats
+    [help-page (:stats app-state)]
 
     [:div "WHOOPS"]))
